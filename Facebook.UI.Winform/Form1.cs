ef820a684dd01e76422d5bb6a44be8bf7a890430
stefan <xstefaan@gmail.com>
2022-02-02 14:53:33 +0100

    index fix

--- Facebook.UI.Winform/Form1.cs
+++ Facebook.UI.Winform/Form1.cs
@@ -40,7 +40,7 @@ namespace Facebook.UI.Winform
         }
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            object SelectedUser = LstUsers.SelectedIndex;
+            object SelectedUser = LstUsers.SelectedIndex + 1;
             ShowPosts(Convert.ToInt32(SelectedUser));
         }
 
@@ -56,7 +56,7 @@ namespace Facebook.UI.Winform
         }
         private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            object SelectedPost = LstPosts.SelectedIndex;
+            object SelectedPost = LstPosts.SelectedIndex + 1;
             ShowComments(Convert.ToInt32(SelectedPost));
         }
         private void button3_Click(object sender, EventArgs e)
